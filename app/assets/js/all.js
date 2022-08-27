5263777ae1190e53851b9b8fb2766f0f7420b1e8
amanoizumi <a0918233246@gmail.com>
2022-08-27 13:21:47 +0800

    add: navbar menu toggle

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,23 @@
-$(function () {});
+$(function () {
+  function toggleSearchInputFn() {
+    if ($("#searchInput").hasClass("hidden")) {
+      $("#searchInput, #angleLeftBtn").removeClass("hidden");
+      $("#logo").addClass("hidden");
+    } else {
+      $("#searchInput, #angleLeftBtn").addClass("hidden");
+      $("#logo").removeClass("hidden");
+    }
+  }
+
+  $("#toggleSearchInput, #angleLeftBtn").on("click", toggleSearchInputFn);
+
+  function toggleNavDropdownFn() {
+    console.log(1);
+    if ($("#navDropdown").hasClass("hidden")) {
+      $("#navDropdown").removeClass("hidden");
+    } else {
+      $("#navDropdown").addClass("hidden");
+    }
+  }
+  $("#toggleNavDropdown").on("click", toggleNavDropdownFn);
+});
